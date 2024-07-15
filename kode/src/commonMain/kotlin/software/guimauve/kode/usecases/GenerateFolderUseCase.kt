6ec7abf3cad4de8632fff03d7fe1685fcc86ef21
NathanFallet <contact@nathanfallet.me>
2024-07-15 11:24:47 +0200

    fix: recursive package name in folder gen

--- kode/src/commonMain/kotlin/software/guimauve/kode/usecases/GenerateFolderUseCase.kt
+++ kode/src/commonMain/kotlin/software/guimauve/kode/usecases/GenerateFolderUseCase.kt
@@ -6,23 +6,32 @@ class GenerateFolderUseCase(
     private val generateCodeUseCase: IGenerateCodeUseCase,
 ) : IGenerateFolderUseCase {
 
-    override fun invoke(input: FileOrDirectory): FolderContent = when (input) {
+    override fun invoke(input: FileOrDirectory): FolderContent = invoke(input, "")
+
+    private fun invoke(input: FileOrDirectory, parentPackage: String): FolderContent = when (input) {
         is Project -> Folder(input.modules.associate { it.name to invoke(it) } +
                 input.textFiles.map { it.key to invoke(it.value) }.associate { it })
 
         is Module -> Folder(input.sourceSets.associate { it.name to invoke(it) })
         is SourceSet -> Folder(input.directorySets.associate { it.name to invoke(it) })
-        is SourceDirectorySet -> folderFor(input.packages, input.files, input.textFiles)
-        is Package -> folderFor(input.packages, input.files, input.textFiles)
+        is SourceDirectorySet -> folderFor(input.packages, parentPackage, input.files, input.textFiles)
+        is Package -> folderFor(input.packages, parentPackage, input.files, input.textFiles)
         is File -> TextFile(generateCodeUseCase(input))
         is TextFile -> input
     }
 
-    // TODO: Fix recursive package names
-    private fun folderFor(packages: List<Package>, files: Map<String, File>, textFiles: Map<String, TextFile>) = Folder(
-        packages.associate { it.name to invoke(it) }
+    private fun folderFor(
+        packages: List<Package>,
+        parentPackage: String,
+        files: Map<String, File>,
+        textFiles: Map<String, TextFile>,
+    ) = Folder(
+        packages.associate { it.associate(parentPackage) }
                 + files.map { it.key to invoke(it.value) }.associate { it }
                 + textFiles.map { it.key to it.value }.associate { it }
     )
+
+    private fun Package.associate(parentPackage: String) =
+        name.removePrefix(parentPackage).replace('.', '/').trim('/') to invoke(this, name)
 
 }
